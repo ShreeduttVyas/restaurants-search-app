cd86daca0361b2fe31b8c5b8be1b0b309df32e03
ShreeduttVyas <113639298+ShreeduttVyas@users.noreply.github.com>
2022-09-26 23:52:38 -0400

    Created global Style and added Images

--- source/Components/Search.js
+++ source/Components/Search.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import { Fontisto } from "@expo/vector-icons"
-
+import { elevation } from "../Shared/Styles";
 const Search = () => {
     return(
-        <View style={styles.container}>
+        <View style={[styles.container, elevation]}>
             <Fontisto name="search" size={25}/>
             <TextInput 
                 placeholder="Restaurants, food"  
@@ -21,10 +21,6 @@ export default Search;
         marginTop: 5,
         marginHorizontal: 5,
         backgroundColor: "white",
-        shadowColor: "black",
-        shadowOffset: {width: 5, height: 5 },
-        elevation: 3,
-        shadowOpacity: 0.1,
         padding: 15,
         borderRadius: 40,
     },
