d9ec24881f75841946a91336b8503e9de1dc8b83
ShreeduttVyas <113639298+ShreeduttVyas@users.noreply.github.com>
2022-10-06 02:31:50 -0400

    Added more routes

--- source/navigation/SigninStack.js
+++ source/navigation/SigninStack.js
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import React from "react";
+import HomeScreen from '../screens/HomeScreen';
+import RestaurantScreen from '../screens/RestaurantScreen'
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +11,12 @@ const SigninStack = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Login" component={LoginScreen} options={{
+                headerShown: false,
+            }} />
+            <Stack.Screen name="Home" component={HomeScreen} options={{
+                headerShown: false,
+            }} />
+               <Stack.Screen name="Restaurant" component={RestaurantScreen} options={{
                 headerShown: false,
             }} />
             <Stack.Screen name="Signup" component={SignupScreen} options={{
